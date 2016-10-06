3bffc990c3df29158cedd639ba20d4c480e61f58
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-10-06 17:28:02 +0100

    Checkpoint: working http request to ssl enabled public url

--- index.android.js
+++ index.android.js
@@ -92,5 +92,32 @@ class BadInstagramCloneApp extends Component {
   }
 }
 
-// AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
-AppRegistry.registerComponent('AwesomeProject', () => BadInstagramCloneApp);
+class HttpTestApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { nice: '' }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome} onPress={this.pressed}>
+          Sup son {this.state.nice}
+        </Text>
+      </View>
+    )
+  }
+
+  pressed = () => {
+    fetch("https://superseriouscompany.com/dias", {
+      method: 'POST'
+    }).then((response) => {
+      debugger;
+      this.setState({nice: response.json().good});
+    }, (error) => {
+      debugger;
+    })
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => HttpTestApp);
