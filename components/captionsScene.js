4a0757c8c93547385833894864bd3fa2b5809f81
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-10-09 22:09:51 +0100

    Checkpoint: UI for scroll view

--- components/captionsScene.js
+++ components/captionsScene.js
@@ -2,17 +2,53 @@ import React, { Component } from 'react';
 
 import {
   View,
-  Text
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Dimensions
 } from 'react-native';
 
 class CaptionsScene extends Component {
+
   render() {
+    const imageHeight = 420, imageWidth = 420;
+
     return (
-      <View style={{padding: 20}}>
-        <Text>Yoyoyo</Text>
+      <View style={styles.container}>
+        <Image style={{width: Dimensions.get('window').width, height: Dimensions.get('window').width * (imageHeight / imageWidth)}} source={{uri: `https://placehold.it/${imageWidth}x${imageHeight}`}} />
+        <ScrollView style={{backgroundColor: 'gray'}}>
+          <Image source={require('../images/Play.png')}/>
+          <Text>Yoyoyo</Text>
+          <Image source={require('../images/Play.png')}/>
+          <Text>Yoyoyo</Text>
+          <Image source={require('../images/Play.png')}/>
+          <Text>Yoyoyo</Text>
+          <Image source={require('../images/Play.png')}/>
+          <Text>Yoyoyo</Text>
+          <Image source={require('../images/Play.png')}/>
+          <Text>Yoyoyo</Text>
+          <Image source={require('../images/Play.png')}/>
+          <Text>Yoyoyo</Text>
+          <Image source={require('../images/Play.png')}/>
+          <Text>Yoyoyo</Text>
+          <Image source={require('../images/Play.png')}/>
+          <Text>Yoyoyo</Text>
+          <Image source={require('../images/Play.png')}/>
+          <Text>Yoyoyo</Text>
+          <Image source={require('../images/Play.png')}/>
+          <Text>Yoyoyo</Text>
+        </ScrollView>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    flex: 1
+  },
+})
 
 module.exports = CaptionsScene;
