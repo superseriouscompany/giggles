20a7efa0dd784967a305628948af12044e30a3c1
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-10-09 22:20:02 +0100

    Render children dynamically

--- components/captionsScene.js
+++ components/captionsScene.js
@@ -14,30 +14,49 @@ class CaptionsScene extends Component {
   render() {
     const imageHeight = 420, imageWidth = 420;
 
+    const captions = [
+      'cool',
+      'nice',
+      'good',
+      'great',
+      'grand',
+      'cool',
+      'nice',
+      'good',
+      'great',
+      'grand',
+      'cool',
+      'nice',
+      'good',
+      'great',
+      'grand',
+      'cool',
+      'nice',
+      'good',
+      'great',
+      'grand',
+      'cool',
+      'nice',
+      'good',
+      'great',
+      'grand',
+      'cool',
+      'nice',
+      'good',
+      'great',
+      'grand'
+    ]
+
     return (
       <View style={styles.container}>
         <Image style={{width: Dimensions.get('window').width, height: Dimensions.get('window').width * (imageHeight / imageWidth)}} source={{uri: `https://placehold.it/${imageWidth}x${imageHeight}`}} />
-        <ScrollView style={{backgroundColor: 'gray'}}>
-          <Image source={require('../images/Play.png')}/>
-          <Text>Yoyoyo</Text>
-          <Image source={require('../images/Play.png')}/>
-          <Text>Yoyoyo</Text>
-          <Image source={require('../images/Play.png')}/>
-          <Text>Yoyoyo</Text>
-          <Image source={require('../images/Play.png')}/>
-          <Text>Yoyoyo</Text>
-          <Image source={require('../images/Play.png')}/>
-          <Text>Yoyoyo</Text>
-          <Image source={require('../images/Play.png')}/>
-          <Text>Yoyoyo</Text>
-          <Image source={require('../images/Play.png')}/>
-          <Text>Yoyoyo</Text>
-          <Image source={require('../images/Play.png')}/>
-          <Text>Yoyoyo</Text>
-          <Image source={require('../images/Play.png')}/>
-          <Text>Yoyoyo</Text>
-          <Image source={require('../images/Play.png')}/>
-          <Text>Yoyoyo</Text>
+        <ScrollView style={styles.scrollContainer}>
+          {captions.map((c, i) => (
+            <View key={i} style={styles.row}>
+              <Image source={require('../images/Play.png')}/>
+              <Text style={styles.text}>{c}</Text>
+            </View>
+          ))}
         </ScrollView>
       </View>
     )
@@ -49,6 +68,19 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     flex: 1
   },
+
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  scrollContainer: {
+    backgroundColor: '#dfdfdf'
+  },
+
+  text: {
+    color: '#666'
+  }
 })
 
 module.exports = CaptionsScene;
