7cb641d99d224d7fbb03ea7206dd5b7563acaf14
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-10-11 11:54:46 +0100

    Add camera permissions for Android

--- android/app/src/main/java/com/steffigraffiti/MainApplication.java
+++ android/app/src/main/java/com/steffigraffiti/MainApplication.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import com.facebook.react.ReactApplication;
 import com.imagepicker.ImagePickerPackage;
 import com.rnim.rn.audio.ReactNativeAudioPackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,8 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new ImagePickerPackage(),
-            new ReactNativeAudioPackage(),
-            new RCTCameraPackage()
+            new ReactNativeAudioPackage()
       );
     }
   };
