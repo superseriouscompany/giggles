b03a6d5b36debd60a87da20934ed7074e484c55f
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-10-13 19:31:43 +0100

    Add react native billing

--- components/submissionScene.android.js
+++ components/submissionScene.android.js
@@ -2,16 +2,32 @@ import React, {Component} from 'react';
 
 import {
   Text,
+  TouchableHighlight,
   View
 } from 'react-native';
+
+const InAppBilling = require("react-native-billing");
 
 class SubmissionScene extends Component {
   render() {
     return (
       <View style={{padding: 20}}>
-        <Text>Hello</Text>
+        <TouchableHighlight onPress={this.buy.bind(this)}>
+          <Text>Hello</Text>
+        </TouchableHighlight>
       </View>
     )
+  }
+
+  buy() {
+    InAppBilling.open().then(() => {
+      InAppBilling.purchase('android.test.purchased');
+    }).then((details) => {
+      console.log("You purchased: ", details)
+      return InAppBilling.close()
+    }).catch((err) => {
+      console.error(err)
+    })
   }
 }
 
