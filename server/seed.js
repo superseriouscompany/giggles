532ad9aa4f2b0965600736b1db1ac1b3f6fd9a20
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-10-20 13:50:15 +0100

    Clean up seed script

--- server/seed.js
+++ server/seed.js
@@ -3,41 +3,19 @@
 const request = require('request');
 const fs      = require('fs');
 
-request({
-  method: 'POST',
-  url: 'http://localhost:3000/submissions',
-  formData: {
-    photo: fs.createReadStream('./fixtures/photo3.jpg'),
-  }
-}, function(err, resp, body) {
-  if( err ) { throw err; }
+for( var i = 1; i <= 5; i++ ) {
+  request({
+    method: 'POST',
+    url: 'http://localhost:3000/submissions',
+    formData: {
+      photo: fs.createReadStream(`./fixtures/photo${i}.jpg`)
+    }
+  }, function(err, resp, body) {
+    if( err ) { throw err; }
 
-  console.log(resp.statusCode, body);
-})
-
-request({
-  method: 'POST',
-  url: 'http://localhost:3000/submissions',
-  formData: {
-    photo: fs.createReadStream('./fixtures/photo4.jpg'),
-  }
-}, function(err, resp, body) {
-  if( err ) { throw err; }
-
-  console.log(resp.statusCode, body);
-})
-
-request({
-  method: 'POST',
-  url: 'http://localhost:3000/submissions',
-  formData: {
-    photo: fs.createReadStream('./fixtures/photo5.jpg'),
-  }
-}, function(err, resp, body) {
-  if( err ) { throw err; }
-
-  console.log(resp.statusCode, body);
-})
+    console.log(resp.statusCode, body);
+  })
+}
 
 setTimeout(function() {
   request({
