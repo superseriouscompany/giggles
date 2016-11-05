7f39b5470cb739744c1b943447a92f4babddc225
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-11-05 13:59:43 +0000

    Add scratch component

---
+++ components/scratch.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React, {Component} from 'react';
+
+import {
+  Text,
+  View
+} from 'react-native';
+
+export default class Scratch extends Component {
+  render() { return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  )}
+}
